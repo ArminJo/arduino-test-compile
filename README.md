cc39eaf263a023750a0dfc3da1f558d2ccd81016
Armin <armin.joachimsmeyer@gmail.com>
2020-03-29 08:31:37 +0200

    Version v2.0.0

--- README.md
+++ README.md
@@ -45,23 +45,23 @@ Sample URL's are:
 
 
 ### `required-libraries`
-List of library dependencies to install. You may add a version number like `@1.3.4`<br/>
+Comma separated list of library dependencies to install. You may add a version number like `@1.3.4`<br/>
 Default `""`<br/>
 Environment name for script usage `ENV_REQUIRED_LIBRARIES`
 
 ```yaml
-required-libraries: Servo "Adafruit NeoPixel@1.3.4"
+required-libraries: Servo,Adafruit NeoPixel@1.3.4
 ```
 
 Space separated list without double quotes around the list. If you need a library with a space in its name, like Adafruit NeoPixel or Adafruit INA219, you must use double quotes around the name and have at least 2 entries, where the first must be without double quotes! You may use Servo as dummy entry.
 
 
 ### `examples-exclude`
-Examples to be **excluded from build**. Space separated list of (unique substrings of) sketch / example names to exclude in build.<br/>
+Examples to be **excluded from build**. Comma or space separated list of (unique substrings of) sketch / example names to exclude in build.<br/>
 Environment name for script usage `ENV_EXAMPLES_EXCLUDE`
 
 ```yaml
-  examples-exclude: QuadrupedControl RobotArmControl # QuadrupedControl and RobotArmControl because of missing EEprom
+  examples-exclude: QuadrupedControl,RobotArmControl # QuadrupedControl and RobotArmControl because of missing EEprom
 ```
 
 ### `examples-build-properties`
@@ -69,7 +69,12 @@ Build parameter like `-DDEBUG` for each example<br/>
 Environment name for script usage `ENV_EXAMPLES_BUILD_PROPERTIES`
 
 ```yaml
-  examples-build-properties: QuadrupedControl RobotArmControl # QuadrupedControl and RobotArmControl because of missing EEprom
+examples-build-properties:
+  WhistleSwitch:
+    -DDEBUG
+    -DFREQUENCY_RANGE_LOW
+  SimpleFrequencyDetector:
+    -DINFO
 ```
 
 ### `cli-version`
@@ -81,16 +86,16 @@ Environment name for script usage `ENV_CLI_VERSION`
 cli-version: 0.9.0 # The current one (3/2020)
 ```
 
-### `sketch-name`
-Pattern or filename of the sketch(es) to test compile. Useful if the sketch is a *.cpp or *.c file or only one sketch in the repository should be compiled.<br/>
+### `sketch-names`
+Comma sepatated list of patterns or filenames (without path) of the sketch(es) to test compile. Useful if the sketch is a *.cpp or *.c file or only one sketch in the repository should be compiled. If first is a `*` the list must be enclosed in double quotes!<br/>
 Default `*.ino`<br/>
-Environment name for script usage `ENV_SKETCH_NAME`
+Environment name for script usage `ENV_SKETCH_NAMES`
 
 ```yaml
-sketch-name: SimpleTouchScreenDSO.cpp
+sketch-names: "*.ino,SimpleTouchScreenDSO.cpp"
 ```
-If the sketch is not contained in a directory with the same name as the sketch, this directory will be created internally and the content of the sketch directory will be recursively copied to it. This is required by arduino-cli to successful compile a sketch.
-
+If the sketch is not contained in a directory with the same name as the sketch, this directory will be created internally and the content of the sketch directory will be recursively copied to it. This is required by arduino-cli to successful compile a sketch.<br/>
+The sketches to compile are internally searched by the command `find . -name "$SKETCH_NAME"` with `.` as the root of the repository, so you do not need to specify the full path.
 
 ### `arduino-platform`
 Comma separated list of platform specifies with optional version. Useful if you require multiple platforms for your board or a fixed version like `arduino:avr@1.8.2`.<br/>
@@ -116,7 +121,7 @@ jobs:
     - name: Checkout
       uses: actions/checkout@master
     - name: Compile all examples
-      uses: ArminJo/arduino-test-compile@v1.1.0
+      uses: ArminJo/arduino-test-compile@v2.0.0
 ```
 
 ## One ESP8266 board with parameter
@@ -133,12 +138,13 @@ jobs:
       uses: actions/checkout@master
       
     - name: Compile all examples
-      uses: ArminJo/arduino-test-compile@v1.1.0
+        uses: ArminJo/arduino-test-compile@v2.0.0
       with:
         arduino-board-fqbn: esp8266:esp8266:huzzah:eesz=4M3M,xtal=80
         platform-url: https://arduino.esp8266.com/stable/package_esp8266com_index.json
-        required-libraries: Servo "Adafruit NeoPixel"
+        required-libraries: Servo,Adafruit NeoPixel
         examples-exclude: WhistleSwitch 50Hz
+        examples-build-properties: "{ \"WhistleSwitch\": \"-DDEBUG -DFREQUENCY_RANGE_LOW\", \"SimpleFrequencyDetector\": \"-DINFO\" }"        
 ```
 
 ## Multiple boards with parameter
@@ -150,7 +156,7 @@ jobs:
     name: ${{ matrix.arduino-boards-fqbn }} - test compiling examples
     runs-on: ubuntu-latest
     env:
-      REQUIRED_LIBRARIES: Servo "Adafruit NeoPixel"
+      REQUIRED_LIBRARIES: Servo,Adafruit NeoPixel
     strategy:
       matrix:
         arduino-boards-fqbn:
@@ -160,7 +166,8 @@ jobs:
 
         include:
           - arduino-boards-fqbn: arduino:avr:uno
-            examples-exclude: 50Hz # Space separated list of (unique substrings of) example names to exclude in build
+            sketch-names: "*.ino" # is default setting
+            examples-exclude: 50Hz # Comma separated list of (unique substrings of) example names to exclude in build
             examples-build-properties:
               WhistleSwitch:
                 -DDEBUG
@@ -169,7 +176,7 @@ jobs:
                 -DINFO
 
           - arduino-boards-fqbn: arduino:avr:uno|trace # UNO board with different build properties
-            examples-exclude: 50Hz # Space separated list of (unique substrings of) example names to exclude in build
+            examples-exclude: 50Hz # Comma separated list of (unique substrings of) example names to exclude in build
             examples-build-properties:
               WhistleSwitch:
                 -DDEBUG
@@ -177,7 +184,7 @@ jobs:
 
           - arduino-boards-fqbn: esp8266:esp8266:huzzah:eesz=4M3M,xtal=80
             platform-url: https://arduino.esp8266.com/stable/package_esp8266com_index.json
-            examples-exclude: WhistleSwitch 50Hz SimpleFrequencyDetector          
+            examples-exclude: WhistleSwitch,50Hz,SimpleFrequencyDetector          
 
       fail-fast: false
                 
@@ -186,7 +193,7 @@ jobs:
         uses: actions/checkout@master
       
       - name: Compile all examples
-        uses: ArminJo/arduino-test-compile@v1.1.0
+        uses: ArminJo/arduino-test-compile@v2.0.0
         with:
           arduino-board-fqbn: ${{ matrix.arduino-boards-fqbn }}
           platform-url: ${{ matrix.platform-url }}
@@ -206,7 +213,7 @@ jobs:
     name: ${{ matrix.arduino-boards-fqbn }} - test compiling examples
     runs-on: ubuntu-latest
     env:
-      REQUIRED_LIBRARIES: Servo "Adafruit NeoPixel"
+      REQUIRED_LIBRARIES: Adafruit NeoPixel,Servo
     strategy:
       matrix:
         arduino-boards-fqbn:
@@ -216,7 +223,7 @@ jobs:
 
         include:
           - arduino-boards-fqbn: arduino:avr:uno
-            examples-exclude: 50Hz # Space separated list of (unique substrings of) example names to exclude in build
+            sketch-names: WhistleSwitch.ino,SimpleFrequencyDetector.ino # Comma separated list of sketch names (no path required) or patterns to use in build
             examples-build-properties:
               WhistleSwitch:
                 -DDEBUG
@@ -225,7 +232,7 @@ jobs:
                 -DINFO
 
           - arduino-boards-fqbn: arduino:avr:uno|trace # UNO board with different build properties
-            examples-exclude: 50Hz # Space separated list of (unique substrings of) example names to exclude in build
+            examples-exclude: 50Hz # Comma separated list of (unique substrings of) example names to exclude in build
             examples-build-properties:
               WhistleSwitch:
                 -DDEBUG
@@ -233,7 +240,7 @@ jobs:
 
           - arduino-boards-fqbn: esp8266:esp8266:huzzah:eesz=4M3M,xtal=80
             platform-url: https://arduino.esp8266.com/stable/package_esp8266com_index.json
-            examples-exclude: WhistleSwitch 50Hz SimpleFrequencyDetector          
+            examples-exclude: WhistleSwitch,50Hz,SimpleFrequencyDetector          
 
       fail-fast: false
       steps:
@@ -253,18 +260,26 @@ jobs:
           ./arduino-test-compile.sh
 ```
             
-Other samples:
+Samples for using action in workflow:
 - The simple example from above. LightweightServo [![Build Status](https://github.com/ArminJo/LightweightServo/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/LightweightServo/blob/master/.github/workflows/LibraryBuild.yml)
 - One sketch, one library. Simple-DSO [![Build Status](https://github.com/ArminJo/Arduino-Simple-DSO/workflows/TestCompile/badge.svg)](https://github.com/ArminJo/Arduino-Simple-DSO/blob/master/.github/workflows/TestCompile.yml)
-- One sketch, one board, multiple options. RobotCar [![Build Status](https://github.com/ArminJo/Arduino-RobotCar/workflows/TestCompile/badge.svg)](https://github.com/ArminJo/Arduino-RobotCar/blob/master/.github/workflows/TestCompile.yml)
 - Arduino library, only arduino:avr boards. Talkie [![Build Status](https://github.com/ArminJo/Talkie/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/Talkie/blob/master/.github/workflows/LibraryBuild.yml)
 - Arduino library, 2 boards. Arduino-FrequencyDetector [![Build Status](https://github.com/ArminJo/Arduino-FrequencyDetector/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/Arduino-FrequencyDetector/blob/master/.github/workflows/LibraryBuildWithAction.yml)
+
+Samples for using `arduino-test-compile.sh script` instead of `ArminJo/arduino-test-compile@v2.0.0` action:
+- One sketch, one board, multiple options. RobotCar [![Build Status](https://github.com/ArminJo/Arduino-RobotCar/workflows/TestCompile/badge.svg)](https://github.com/ArminJo/Arduino-RobotCar/blob/master/.github/workflows/TestCompile.yml)
 - Arduino library, multiple boards. ServoEasing [![Build Status](https://github.com/ArminJo/ServoEasing/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/ServoEasing/blob/master/.github/workflows/LibraryBuild.yml)
-- Using script, Arduino library, multiple boards. NeoPatterns [![Build Status](https://github.com/ArminJo/NeoPatterns/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/NeoPatterns/blob/master/.github/workflows/LibraryBuild.yml)
+- Arduino library, multiple boards. NeoPatterns [![Build Status](https://github.com/ArminJo/NeoPatterns/workflows/LibraryBuild/badge.svg)](https://github.com/ArminJo/NeoPatterns/blob/master/.github/workflows/LibraryBuild.yml)
 
 # Revision History
+### Version 2.0.0
+- Changed `required-libraries` from **space** to **comma** separated list.
+- Renamed parameter `sketch-name` to `sketch-names` to enable comma separated list.
+- Accept comma separated list for `examples-exclude`.
+- Updated documentation.
+
 ### Version 1.1.0
-- Renamed parameter `libraries` to `required-libraries`
+- Renamed parameter `libraries` to `required-libraries`.
 - Renamed script.
 - Script instead of action can be used in steps.
 - Added parameter `arduino-platform` to enable specifying the version of the required platform.
--- arduino-test-compile.sh
+++ arduino-test-compile.sh
@@ -10,7 +10,7 @@
 
 # Input parameter
 CLI_VERSION="$1"
-SKETCH_NAME="$2"
+SKETCH_NAMES="$2"
 ARDUINO_BOARD_FQBN="$3"
 ARDUINO_PLATFORM="$4"
 PLATFORM_URL="$5"
@@ -28,7 +28,7 @@ readonly BLUE='\033[0;34m'
 # Get env parameter with higher priority, which enables the script to run directly in a step
 #
 if [[ -n $ENV_CLI_VERSION ]]; then CLI_VERSION=$ENV_CLI_VERSION; fi
-if [[ -n $ENV_SKETCH_NAME ]]; then SKETCH_NAME=$ENV_SKETCH_NAME; fi
+if [[ -n $ENV_SKETCH_NAMES ]]; then SKETCH_NAMES=$ENV_SKETCH_NAMES; fi
 if [[ -n $ENV_ARDUINO_BOARD_FQBN ]]; then ARDUINO_BOARD_FQBN=$ENV_ARDUINO_BOARD_FQBN; fi
 if [[ -n $ENV_ARDUINO_PLATFORM ]]; then ARDUINO_PLATFORM=$ENV_ARDUINO_PLATFORM; fi
 if [[ -n $ENV_PLATFORM_URL ]]; then PLATFORM_URL=$ENV_PLATFORM_URL; fi
@@ -42,7 +42,7 @@ if [[ -n $ENV_DEBUG ]]; then DEBUG=$ENV_DEBUG; fi
 # Enforce defaults. Required at least for script version. !!! MUST be equal the defaults in action.yml !!!
 #
 if [[ -z $CLI_VERSION ]]; then echo "Set CLI_VERSION to default value: \"latest\""; CLI_VERSION='latest'; fi
-if [[ -z $SKETCH_NAME ]]; then echo -e "Set SKETCH_NAME to default value: \"*.ino\""; SKETCH_NAME='*.ino'; fi
+if [[ -z $SKETCH_NAMES ]]; then echo -e "Set SKETCH_NAMES to default value: \"*.ino\""; SKETCH_NAMES='*.ino'; fi
 if [[ -z $ARDUINO_BOARD_FQBN ]]; then echo "Set ARDUINO_BOARD_FQBN to default value: \"arduino:avr:uno\""; ARDUINO_BOARD_FQBN='arduino:avr:uno'; fi
 
 
@@ -51,7 +51,7 @@ if [[ -z $ARDUINO_BOARD_FQBN ]]; then echo "Set ARDUINO_BOARD_FQBN to default va
 #
 echo -e "\n\n"$YELLOW"Echo input parameter"
 echo CLI_VERSION=$CLI_VERSION
-echo SKETCH_NAME=$SKETCH_NAME
+echo SKETCH_NAMES=$SKETCH_NAMES
 echo ARDUINO_BOARD_FQBN=$ARDUINO_BOARD_FQBN
 echo ARDUINO_PLATFORM=$ARDUINO_PLATFORM
 echo PLATFORM_URL=$PLATFORM_URL
@@ -113,7 +113,7 @@ else
   PLATFORM=$ARDUINO_PLATFORM
 fi
 echo PLATFORM=${PLATFORM}
-if [[ ${PLATFORM} != *"arduino"* && -z $PLATFORM_URL ]]; then
+if [[ ${PLATFORM} != *arduino* && -z $PLATFORM_URL ]]; then
   echo -e "::error::Non Arduino platform $PLATFORM requested, but \"platform-url\" parameter is missing."
   exit 1
 fi
@@ -123,30 +123,23 @@ if [[ -n $PLATFORM_URL ]]; then
   PLATFORM_URL_COMMAND="--additional-urls"
 fi
 
-PLATFORM=${PLATFORM//,/ } # replace comma by space to enable multiple platforms which are comma separated
-PLATFORM_ARRAY=( $PLATFORM )
+PLATFORM=${PLATFORM//,/ } # replace all comma by space to enable multiple platforms which are comma separated
+declare -a PLATFORM_ARRAY=( $PLATFORM )
 #declare -p PLATFORM_ARRAY # print properties of PLATFORM_ARRAY
 for single_platform in "${PLATFORM_ARRAY[@]}"; do # Loop over all platforms specified
-#  if [[ -z $PLATFORM_URL ]]; then
-#    echo -e "arduino-cli core update-index > /dev/null"
-#    arduino-cli core update-index > /dev/null
-#    echo "arduino-cli core install $single_platform > /dev/null"
-#    arduino-cli core install $single_platform > /dev/null
-#  else
     echo -e "arduino-cli core update-index $PLATFORM_URL_COMMAND $PLATFORM_URL > /dev/null"
     arduino-cli core update-index $PLATFORM_URL_COMMAND $PLATFORM_URL > /dev/null # must specify --additional-urls here
     echo -e "arduino-cli core install $single_platform $PLATFORM_URL_COMMAND $PLATFORM_URL > /dev/null"
     arduino-cli core install $single_platform $PLATFORM_URL_COMMAND $PLATFORM_URL > /dev/null
-#  fi
 done
 
-if [[ "${PLATFORM}" == "esp8266:esp8266" && ! -f /usr/bin/python3 ]]; then ## double brackets required
-  # /github/home/.arduino15/packages/esp8266/tools/python3/3.7.2-post1/python3 -> /usr/bin/python3
+if [[ ${PLATFORM} == esp8266:esp8266 && ! -f /usr/bin/python3 ]]; then
+  # python3 is a link in the esp8266 package: /github/home/.arduino15/packages/esp8266/tools/python3/3.7.2-post1/python3 -> /usr/bin/python3
   echo "install python3 for ESP8266"
   apt-get install -qq python3 > /dev/null
 fi
           
-if [[ "$PLATFORM" == "esp32:esp32" ]]; then
+if [[ $PLATFORM == esp32:esp32 ]]; then
   if [[ ! -f /usr/bin/pip ]]; then
     echo "install python and pip for ESP32"
     apt-get install -qq python-pip > /dev/null # this installs also python
@@ -159,15 +152,18 @@ arduino-cli board listall
 
 
 #
-# Install libraries if needed
+# Install required libraries
 #
-echo -e "\n"$YELLOW"Install libraries if needed"
+echo -e "\n"$YELLOW"Install required libraries"
 if [[ -z $REQUIRED_LIBRARIES ]]; then
   echo "No additional libraries to install"
 else
-  echo -n "Install libraries $REQUIRED_LIBRARIES "
-  # Support library names which contain whitespace
-  declare -a -r REQUIRED_LIBRARIES_ARRAY="(${REQUIRED_LIBRARIES})"
+  echo -n "Install libraries $REQUIRED_LIBRARIES"
+  BACKUP_IFS="$IFS"
+  # Split comma separated library list
+  IFS=$','
+  declare -a REQUIRED_LIBRARIES_ARRAY=( $REQUIRED_LIBRARIES )
+  IFS="$BACKUP_IFS"
   arduino-cli lib install "${REQUIRED_LIBRARIES_ARRAY[@]}"
   if [[ $? -ne 0 ]]; then
     echo "::error::Installation of "$REQUIRED_LIBRARIES" failed"
@@ -175,66 +171,76 @@ else
   fi
 fi
 
+
 #
-# Finally, we compile all examples
+# Get the build property map
 #
 echo -e "\n"$YELLOW"Compiling sketches / examples for board $ARDUINO_BOARD_FQBN \n"
 
 # If matrix.examples-build-properties are specified, create an associative shell array
-EXAMPLES_BUILD_PROPERTIES=${EXAMPLES_BUILD_PROPERTIES#\{} # remove the "{". The "}" is required as end token
 if [[ -n $EXAMPLES_BUILD_PROPERTIES && $EXAMPLES_BUILD_PROPERTIES != "null" ]]; then # contains "null", if passed as environment variable
   echo EXAMPLES_BUILD_PROPERTIES=$EXAMPLES_BUILD_PROPERTIES
+  EXAMPLES_BUILD_PROPERTIES=${EXAMPLES_BUILD_PROPERTIES#\{} # remove the "{". The "}" is required as end token
   declare -A PROP_MAP="( $(echo $EXAMPLES_BUILD_PROPERTIES | sed -E 's/"(\w*)": *([^,}]*)[,}]/\[\1\]=\2/g' ) )"
   declare -p PROP_MAP # print properties of PROP_MAP
 else
   declare -A PROP_MAP=( [dummy]=dummy )
 fi
 
-
-# Search recursively for *.ino files starting at root
-SKETCHES=($(find . -name "$SKETCH_NAME"))
-for sketch in "${SKETCHES[@]}"; do # Loop over all sketch files
-  SKETCH_PATH=$(dirname $sketch) # complete path to sketch
-  SKETCH_DIR=${SKETCH_PATH##*/}  # directory of sketch, must match sketch basename
-  SKETCH_FILENAME=$(basename $sketch) # complete name of sketch
-  SKETCH_EXTENSION=${SKETCH_FILENAME##*.} # extension of sketch
-  SKETCH_BASENAME=${SKETCH_FILENAME%%.*} # name wihout extension / basename of sketch, must match directory name
-  if [[ "$EXAMPLES_EXCLUDE" == *"$SKETCH_BASENAME"* ]]; then
-    echo -e "Skipping $SKETCH_BASENAME \xe2\x9e\x9e" # Right arrow
-  else
-    # If sketch name does not end with .ino, rename it locally
-    if [[ "$SKETCH_EXTENSION" != "ino" ]]; then
-      echo "Rename ${SKETCH_PATH}/${SKETCH_FILENAME} to ${SKETCH_PATH}/${SKETCH_BASENAME}.ino"
-      mv ${SKETCH_PATH}/${SKETCH_FILENAME} ${SKETCH_PATH}/${SKETCH_BASENAME}.ino
-    fi
-    # If directory name does not match sketch name, create an appropriate directory, copy the files recursively and compile
-    if [[ "$SKETCH_DIR" != "$SKETCH_BASENAME" ]]; then
-      mkdir $HOME/$SKETCH_BASENAME
-      echo "Creating directory $HOME/$SKETCH_BASENAME and copy ${SKETCH_PATH}/* to it"
-      cp -R ${SKETCH_PATH}/* $HOME/$SKETCH_BASENAME
-      SKETCH_PATH=$HOME/$SKETCH_BASENAME
-    fi
-    # check if there is an entry in the associative array and create compile parameter to put in compiler.cpp.extra_flags
-    echo -n "Compiling $SKETCH_BASENAME "
-    if [[ "${PROP_MAP[$SKETCH_BASENAME]}" != "" ]]; then
-      echo -n "with ${PROP_MAP[$SKETCH_BASENAME]} "
-    fi
-    if [[ "$DEBUG" == "true" ]]; then
-      arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags="${PROP_MAP[$SKETCH_BASENAME]}" $SKETCH_PATH
+#
+# Finally, we compile all examples
+#
+# Support comma separated sketch name list
+BACKUP_IFS="$IFS"
+IFS=$','
+declare -a SKETCH_NAMES_ARRAY=( $SKETCH_NAMES )
+IFS="$BACKUP_IFS"
+for sketch_name in "${SKETCH_NAMES_ARRAY[@]}"; do # Loop over all sketch names
+  declare -a SKETCHES=($(find . -name "$sketch_name"))
+  for sketch in "${SKETCHES[@]}"; do # Loop over all sketch files
+    SKETCH_PATH=$(dirname $sketch) # complete path to sketch
+    SKETCH_DIR=${SKETCH_PATH##*/}  # directory of sketch, must match sketch basename
+    SKETCH_FILENAME=$(basename $sketch) # complete name of sketch
+    SKETCH_EXTENSION=${SKETCH_FILENAME##*.} # extension of sketch
+    SKETCH_BASENAME=${SKETCH_FILENAME%%.*} # name wihout extension / basename of sketch, must match directory name
+    if [[ $EXAMPLES_EXCLUDE == *"$SKETCH_BASENAME"* ]]; then
+      echo -e "Skipping $SKETCH_BASENAME \xe2\x9e\x9e" # Right arrow
     else
-      build_stdout=$(arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags="${PROP_MAP[$SKETCH_BASENAME]}" $SKETCH_PATH 2>&1)
-    fi
-    if [[ $? -ne 0 ]]; then
-      echo -e ""$RED"\xe2\x9c\x96" # If ok output a green checkmark else a red X and the command output.
-      echo "::error::Compile of  $SKETCH_BASENAME ${PROP_MAP[$SKETCH_BASENAME]} failed"
-      exit_code=1
-      if [[ "$DEBUG" != "true" ]]; then
-        echo -e "$build_stdout \n"
+      # If sketch name does not end with .ino, rename it locally
+      if [[ $SKETCH_EXTENSION != ino ]]; then
+        echo "Rename ${SKETCH_PATH}/${SKETCH_FILENAME} to ${SKETCH_PATH}/${SKETCH_BASENAME}.ino"
+        mv ${SKETCH_PATH}/${SKETCH_FILENAME} ${SKETCH_PATH}/${SKETCH_BASENAME}.ino
       fi
-    else
-      echo -e ""$GREEN"\xe2\x9c\x93"
+      # If directory name does not match sketch name, create an appropriate directory, copy the files recursively and compile
+      if [[ $SKETCH_DIR != $SKETCH_BASENAME ]]; then
+        mkdir $HOME/$SKETCH_BASENAME
+        echo "Creating directory $HOME/$SKETCH_BASENAME and copy ${SKETCH_PATH}/* to it"
+        cp -R ${SKETCH_PATH}/* $HOME/$SKETCH_BASENAME
+        SKETCH_PATH=$HOME/$SKETCH_BASENAME
+      fi
+      # check if there is an entry in the associative array and create compile parameter to put in compiler.cpp.extra_flags
+      echo -n "Compiling $SKETCH_BASENAME "
+      if [[ -n ${PROP_MAP[$SKETCH_BASENAME]} ]]; then
+        echo -n "with ${PROP_MAP[$SKETCH_BASENAME]} "
+      fi
+      if [[ $DEBUG == true ]]; then
+        echo "Debug mode enabled => compile output will be printed also for successful compilation"
+        arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags="${PROP_MAP[$SKETCH_BASENAME]}" $SKETCH_PATH
+      else
+        build_stdout=$(arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags="${PROP_MAP[$SKETCH_BASENAME]}" $SKETCH_PATH 2>&1)
+      fi
+      if [[ $? -ne 0 ]]; then
+        echo -e ""$RED"\xe2\x9c\x96" # If ok output a green checkmark else a red X and the command output.
+        echo "::error::Compile of  $SKETCH_BASENAME ${PROP_MAP[$SKETCH_BASENAME]} failed"
+        exit_code=1
+        if [[ $DEBUG != true ]]; then
+          echo -e "$build_stdout \n"
+        fi
+      else
+        echo -e ""$GREEN"\xe2\x9c\x93"
+      fi
+      echo "arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags=\"${PROP_MAP[$SKETCH_BASENAME]}\" $SKETCH_PATH"
     fi
-    echo "arduino-cli compile --verbose --warnings all --fqbn ${ARDUINO_BOARD_FQBN%|*} --build-properties compiler.cpp.extra_flags=\"${PROP_MAP[$SKETCH_BASENAME]}\" $SKETCH_PATH"
-  fi
+  done
 done
 exit $exit_code
